b3b12eb50f940c49ca6fbfad371bc5d51262086e
Edward Yang <edward.yang@magnet.com>
2016-03-16 15:17:03 -0700

    restructure sample dir

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express')
 , app = express()
 , http = require('http');
 
-app.set('port', 3001);
+app.set('port', 3000);
 
 app.configure(function(){
-    app.use(express.static(__dirname + '/../web'));
+    app.use(express.static(__dirname + '/../'));
 });
 
 http.createServer(app).listen(app.get('port'), function(){
