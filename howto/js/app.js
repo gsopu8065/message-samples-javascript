4e6ef8fe095d0f6846a7dae83a876c14d5da44f9
Edward Yang <edward.yang@magnet.com>
2016-03-25 10:38:04 -0700

    add baseurl

--- howto/js/app.js
+++ howto/js/app.js
@@ -1,8 +1,12 @@
 // initialize the messaging SDK
 Max.init({
     clientId: '<your client id>',
-    clientSecret: '<your client secret>'
+    clientSecret: '<your client secret>',
+    baseUrl: 'https://sandbox.magnet.com/mobile/api'
 });
+
+Max.Config.payloadLogging = true;
+Max.Config.logLevel = 'FINE';
 
 // application logic should occur after onReady() is fired
 Max.onReady(function() {
