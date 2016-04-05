074db1c2831d10e28cb92186d78012a06558ab32
Edward Yang <edward.yang@magnet.com>
2016-04-04 20:26:12 -0700

    leave process open in background

--- messenger-desktop/app/background.js
+++ messenger-desktop/app/background.js
@@ -4,14 +4,15 @@
 // window from here.
 
 import { app } from 'electron';
-import devHelper from './helpers/dev';
 import createWindow from './helpers/window';
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
 import env from './env';
 
+var Menu = require('menu');
 var mainWindow;
+var force_quit = false;
 
 app.on('ready', function () {
 
@@ -20,17 +21,81 @@ app.on('ready', function () {
         height: 600
     });
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools();
-
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    if (env.name !== 'production') {
-        devHelper.setDevMenu();
+    if (env.name == 'production') {
+        var menu = Menu.buildFromTemplate([{
+            label: 'Magnet Messenger',
+            submenu: [{
+                label: 'Quit',
+                accelerator: 'CmdOrCtrl+Q',
+                click: function() {
+                    force_quit = true;
+                    app.quit();
+                }
+            }]
+        }]);
+        Menu.setApplicationMenu(menu);
+    } else {
+        var devMenu = Menu.buildFromTemplate([{
+            label: 'Development',
+            submenu: [{
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function() {
+                    BrowserWindow.getFocusedWindow().webContents.reloadIgnoringCache();
+                }
+            },{
+                label: 'Toggle DevTools',
+                accelerator: 'Alt+CmdOrCtrl+I',
+                click: function() {
+                    BrowserWindow.getFocusedWindow().toggleDevTools();
+                }
+            },{
+                label: 'Quit',
+                accelerator: 'CmdOrCtrl+Q',
+                click: function() {
+                    force_quit = true;
+                    app.quit();
+                }
+            }]
+        }]);
+        Menu.setApplicationMenu(devMenu);
+
         mainWindow.openDevTools();
     }
+
+    // Continue to handle mainWindow "close" event here
+    mainWindow.on('close', function(e) {
+        if (!force_quit) {
+            e.preventDefault();
+            mainWindow.hide();
+        }
+    });
+
+    // You can use 'before-quit' instead of (or with) the close event
+    app.on('before-quit', function(e) {
+        // Handle menu-item or keyboard shortcut quit here
+        if (!force_quit) {
+            e.preventDefault();
+            mainWindow.hide();
+        }
+    });
+
+    app.on('activate-with-no-open-windows', function(){
+        mainWindow.show();
+    });
+
+    app.on('will-quit', function () {
+        // This is a good place to add tests insuring the app is still
+        // responsive and all windows are closed.
+        mainWindow = null;
+    });
+
+    app.on('window-all-closed', function () {
+        if (process.platform != 'darwin')
+            app.quit();
+    });
+
 });
 
-app.on('window-all-closed', function () {
-    //app.quit();
-});
